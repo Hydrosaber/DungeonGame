4f40361b7963a4123a7a36409700283a8775e1c1
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2020-01-16 10:17:14 -0500

    Set Location

--- character.gd
+++ character.gd
@@ -1,25 +1,26 @@
 extends Node2D
 
-class_name character
-
 var total_health = 0;
 var attack_val;
 var nickname = "";
-var location;
+var coordinates;
 var moves = [];
 var level = 1;
+var Room = preload("res://Room.tscn");
 
 func gen_HP():
 	return 0;
 	
 func get_location():
-	return 0;
+	return coordinates;
 	
 func set_location():
-	return 0;
+	var x = Room.find_start_room().position.x + 1 + randi() % int(Room.find_start_room().size.x - 2);
+	var y = Room.find_start_room().position.y + 1 + randi() % int(Room.find_start_room().size.y - 2);
+	coordinates = Vector2(x, y);
 	
 func move(position):
-	return true;
+	return 0;
 	
 func update_visuals():
 	return 0;
