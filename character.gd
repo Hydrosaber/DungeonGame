9985c6b17de7b564ea3e4dbb6cb32157110ab965
eriqvt <49623106+eriqvt@users.noreply.github.com>
2020-01-16 10:51:44 -0500

    Update character.gd

--- character.gd
+++ character.gd
@@ -3,32 +3,27 @@ extends Node2D
 var total_health = 0;
 var attack_val;
 var nickname = "";
-var coordinates;
+var location;
 var moves = [];
 var level = 1;
-var Room = preload("res://Room.tscn");
 
 func gen_HP():
 	return 0;
-	
+
 func get_location():
-	return coordinates;
-	
-func set_location():
-	
-func move(position):
 	return 0;
-	
+
+func set_location():
+	return 0;
+
+func move(position):
+	return true;
+
 func update_visuals():
 	return 0;
-	
+
 func attack(position):
 	return 0;
-	
+
 func hurt(var amount):
 	return 0;
-
-
-
-
-
